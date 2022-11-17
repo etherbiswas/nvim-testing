2e826264f88633d6e8e24331ff43c65b7801e2ec
Ether <etherrorcode404@gmail.com>
2022-11-17 10:11:10 +0600

    Delete ftplugin directory

--- ftplugin/ruby.lua
+++
@@ -1,5 +0,0 @@
-local options = {
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
-  softtabstop = 2
-}
